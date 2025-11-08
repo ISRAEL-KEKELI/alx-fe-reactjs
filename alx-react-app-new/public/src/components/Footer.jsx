b1e079977c9e11dbf3556ccfdb775a86c80ecb80
KEKELI <israelkekelialorvor@gmail.com>
2025-11-08 13:18:44 +0000

    Delete alx-react-app-new/public directory

--- alx-react-app-new/public/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-return (
-<footer style={{textAlign:'left'}}>
-  <p>© 2023 City Lovers</p>
-</footer>
-);
-}
-
-export default Footer;
--- alx-react-app-new/public/src/components/MainContent.jsx
+++
@@ -1,9 +0,0 @@
-function MainContent() {
-return (
-<main>
-  <p><span style={{fontWeight: 'bold', backgroundColor: 'blue', color: 'white', fontSize: '40px'}}>I love to visit New York, Paris, and Tokyo.</span></p>
-</main>
-);
-}
-
-export default MainContent;
